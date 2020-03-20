643cc9de0538f5c2cfaede3734edba04bed6d275
Rafael Moureira Patrocinio <rafapatroc01@hotmail.com>
2020-03-20 19:00:12 -0300

    Committ

---
+++ Notes.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 76ff83893c5b0d214bd0705080aa51c1676b4bae */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1273
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 333527
+	.include	"typemap.jm.inc"
---
+++ Notes.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 2ddd440c623f426f6bffe4b6c6d1f405e6c33ea7 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1418
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 371517
+	.include	"typemap.mj.inc"
